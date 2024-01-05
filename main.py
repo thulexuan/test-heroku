625d778ec2f1e0d2bd3ae90b8438bcc95eb2fc81
thulexuan <76806313+thulexuan@users.noreply.github.com>
2024-01-05 11:16:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Request, File, UploadFile, BackgroundTasks
-from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
 
 from fastapi import FastAPI
